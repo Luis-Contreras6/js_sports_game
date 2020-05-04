6178db3e7de2ccbd4dd1811afa82161baa4b72a6
Luis <lcontr65@gmail.com>
2020-05-03 20:21:24 -0500

    side two

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,22 @@ let teamTwoNumShots = document.querySelector("#teamtwo-numshots")
 let teamTwoNumGoals = document.querySelector("#teamtwo-numgoals")
 
 teamOneShoot.addEventListener("click", function () {
-let newTeamOneNumShots = Number(teamOneNumShots.innerHTML) + 1;
-let x = Math.floor(Math.random() * 10);
-if (x % 2 == 0) {x = Number(teamOneNumGoals.innerHTML) + 1;
-teamOneNumGoals.innerHTML = x;}
-teamOneNumShots.innerHTML = newTeamOneNumShots;})
+    let newTeamOneNumShots = Number(teamOneNumShots.innerHTML) + 1;
+    let x = Math.floor(Math.random() * 20);
+    if (x % 2 == 0) {
+        x = Number(teamOneNumGoals.innerHTML) + 1;
+        teamOneNumGoals.innerHTML = x;
+    }
+    teamOneNumShots.innerHTML = newTeamOneNumShots;
+})
+
+teamTwoShoot.addEventListener("click", function () {
+    let newTeamTwoNumShots = Number(teamTwoNumShots.innerHTML) + 1;
+    let x = Math.floor(Math.random() * 20);
+    if (x % 2 == 0) {
+        x = Number(teamTwoNumGoals.innerHTML) + 1;
+        teamTwoNumGoals.innerHTML = x;
+    }
+    teamTwoNumShots.innerHTML = newTeamTwoNumShots;
+})
+
